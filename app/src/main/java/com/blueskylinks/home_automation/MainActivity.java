216192a23d7f2ca34d768fa80c0c3ea5f8008897
blueskylinks <blueskylinks@outlook.com>
2018-09-15 17:29:06 +0530

    Initial commit

---
+++ app/src/main/java/com/blueskylinks/home_automation/MainActivity.java
@@ -0,0 +1,128 @@
+package com.blueskylinks.home_automation;
+
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothManager;
+import android.bluetooth.le.BluetoothLeScanner;
+import android.bluetooth.le.ScanCallback;
+import android.bluetooth.le.ScanFilter;
+import android.bluetooth.le.ScanRecord;
+import android.bluetooth.le.ScanResult;
+import android.bluetooth.le.ScanSettings;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+import android.os.ParcelUuid;
+import android.support.annotation.RequiresApi;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.RelativeLayout;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    private BluetoothManager mBluetoothManager;
+    private BluetoothAdapter mBluetoothAdapter;
+    BluetoothLeScanner scanner;
+    private BluetoothDevice ble_device;
+    ScanRecord scan_rec;
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ActivityCompat.requestPermissions(this,
+                new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, 200);
+        ActivityCompat.requestPermissions(this,
+                new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 200);
+        initialize();
+
+    }
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    @Override
+    protected void onResume() {
+        super.onResume();
+      //  startscand();
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    public boolean initialize() {
+        // For API level 18 and above, get a reference to BluetoothAdapter through
+        // BluetoothManager.
+        Log.i("BleScanning:", "initilizing.......");
+        if (mBluetoothManager == null) {
+            mBluetoothManager = (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
+            if (mBluetoothManager == null) {
+                Log.i("BleScanning:", "Unable to initialize BluetoothManager.");
+                return false;
+            }
+
+        }
+
+        mBluetoothAdapter = mBluetoothManager.getAdapter();
+        if (mBluetoothAdapter == null) {
+            Log.i("BleScanning:", "Unable to obtain a BluetoothAdapter.");
+            return false;
+        }
+        if (mBluetoothAdapter == null || !mBluetoothAdapter.isEnabled()) {
+            //Bluetooth is disabled
+            mBluetoothAdapter.enable();
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        startscand();
+        return true;
+    }
+
+    //================  Start BLE Scanning  ===============
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    public void startscand() {
+        Log.i("BLE------", "Start Scanning");
+        //final ParcelUuid UID_SERVICE =
+        ParcelUuid.fromString("000000f1-0000-1000-8000-00805f9b34fb");
+        scanner = mBluetoothAdapter.getBluetoothLeScanner();
+        ScanFilter beaconFilter = new ScanFilter.Builder() // this filter will be used to get only specific device based on service UUID
+                //.setServiceUuid(UID_SERVICE)
+                .build();
+        ArrayList<ScanFilter> filters = new ArrayList<ScanFilter>();
+        filters.add(beaconFilter);
+        ScanSettings settings = new ScanSettings.Builder()
+                .setScanMode(ScanSettings.SCAN_MODE_LOW_LATENCY)
+                .build();
+        scanner.startScan(filters, settings, mcallback);
+    }
+    // This callback method will be automatically called every time the scanner get the device adv data
+    public ScanCallback mcallback = new ScanCallback() {
+        @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+        @Override
+        public void onScanResult(int callbackType, ScanResult result) {
+            int rssi;
+            rssi = result.getRssi();
+            scan_rec = result.getScanRecord();
+            Log.i("Scan result", String.valueOf(rssi));
+            Log.i("record", scan_rec.toString());
+
+        }
+    };
+
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    public void stopscand() {
+        Log.i("BLE-----", "Stop Scanning");
+        scanner.stopScan(mcallback);
+    }
+    public void setting_func(View view){
+        //starting another activity..
+        Intent it4 = new Intent(MainActivity.this, Setting.class);
+        startActivity(it4);
+
+    }
+}
---
+++ app/src/main/java/com/blueskylinks/home_automation/Setting.java
@@ -0,0 +1,195 @@
+package com.blueskylinks.home_automation;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toolbar;
+
+import static com.blueskylinks.home_automation.R.drawable.*;
+
+public class Setting extends AppCompatActivity {
+    private DrawerLayout mDrawerLayout;
+    NavigationView navigationView;
+    public static int navItemIndex =0;
+    ImageView view1;
+    ImageView view2;
+    TextView t1;
+    TextView fanSpeed;
+    TextView bulb1;
+    TextView bulb2;
+    LinearLayout root;
+    SharedPreferences sharedPreferences;
+    SharedPreferences.Editor editor;
+    int count=0;
+    ImageView fan_img[]=new ImageView[5];
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setting);
+        mDrawerLayout = findViewById(R.id.drawer_layout);
+        // Toolbar toolbar = findViewById(R.id.toolbar);
+
+        root=findViewById(R.id.root_layout);
+        ActionBar actionbar = getSupportActionBar();
+        actionbar.setDisplayHomeAsUpEnabled(true);
+       // actionbar.setHomeAsUpIndicator(R.drawable.ic_menu);
+        t1=findViewById(R.id.Room_name);
+        fanSpeed=findViewById(R.id.fan_speed);
+        bulb1=findViewById(R.id.bulb1_status);
+        bulb2=findViewById(R.id.bulb2_status);
+
+        sharedPreferences = getSharedPreferences("MyPREFERENCES", Context.MODE_PRIVATE);
+        editor = sharedPreferences.edit();
+
+        int index=sharedPreferences.getInt("index",0);
+       if(index==1){ t1.setText("Room1");}
+        else if(index==2){ t1.setText("Room2");}
+        else if(index==3) {t1.setText("Room3");}
+        else if(index==4) {t1.setText("Room4");}
+        else return;
+
+        fan_img[0]=findViewById(R.id.fan_img1);
+        fan_img[1]=findViewById(R.id.fan_img2);
+        fan_img[2]=findViewById(R.id.fan_img3);
+        fan_img[3]=findViewById(R.id.fan_img4);
+        fan_img[4]=findViewById(R.id.fan_img5);
+
+
+
+        navigationView = findViewById(R.id.nav_view);
+      navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+
+                        //Check to see which item was being clicked and perform appropriate action
+                        switch (menuItem.getItemId()) {
+                            //Replacing the main content with ContentFragment Which is our Inbox View;
+                            case R.id.nav_camera:
+                                navItemIndex = 1;
+                                t1.setText("Room1");
+                                mDrawerLayout.closeDrawers();
+                                editor.putInt("index",1);
+                                editor.commit();
+                                break;
+                            case R.id.nav_gallery:
+                                navItemIndex = 2;
+                                t1.setText("Room2");
+                                editor.putInt("index",2);
+                                editor.commit();
+                                mDrawerLayout.closeDrawers();
+                                break;
+                            case R.id.nav_slideshow:
+                                // launch new intent instead of loading fragment
+                                // startActivity(new Intent(Setting.this, Room1.class));
+                                t1.setText("Room3");
+                                editor.putInt("index",3);
+                                editor.commit();
+                                mDrawerLayout.closeDrawers();
+                                return true;
+                            case R.id.nav_manage:
+                                // launch new intent instead of loading fragment
+                                //startActivity(new Intent(MainActivity.this, PrivacyPolicyActivity.class));
+                                t1.setText("Room4");
+                                editor.putInt("index",4);
+                                editor.commit();
+                                mDrawerLayout.closeDrawers();
+                                return true;
+                            default:
+                                navItemIndex = 0;
+                        }
+
+                        //Checking if the item is in checked state or not, if not make it in checked state
+                        if (menuItem.isChecked()) {
+                            menuItem.setChecked(false);
+                        } else {
+
+                            menuItem.setChecked(true);
+                        }
+                        menuItem.setChecked(true);
+
+                        return true;
+                    }
+                });
+    }
+
+    //function for fan speed decreament
+  public void decrease_func(View view){
+        view1=findViewById(R.id.decrease);
+      //view1.setBackgroundColor(Color.parseColor("#f1c40f"));
+     if(count>=1){
+         count --;
+          for(int i=0;i<=count;i++){
+              if(count == i){
+                  Log.i("count value",String.valueOf(count));
+                  fan_img[i].setImageDrawable(null);
+                  fanSpeed.setText(String.valueOf(count));
+                  fan_img[i].setBackgroundResource(R.mipmap.fan_black);
+              }
+          }
+      }
+      else return;
+  }
+
+  //function for fan speed increament
+    public void increase_func(View view){
+      view2=findViewById(R.id.increase);
+     // view2.setBackgroundColor(Color.parseColor( "#f1c40f"));
+           if(count<5){
+               for (int i = 0; i <= count; i++) {
+                   if (count == i) {
+                       Log.i("count value",String.valueOf(count));
+                       fan_img[i].setBackgroundResource(R.mipmap.fan_yellow);
+                       fanSpeed.setText(String.valueOf(count+1));
+                   }
+               }
+               count++;
+           }
+           else return;
+    }
+    public void add_layout(){
+        LinearLayout llay1 = new LinearLayout(this);
+//        llay1.getLayoutParams().width=200;
+  //      llay1.getLayoutParams().height=0;
+        llay1.setVisibility(View.VISIBLE);
+        llay1.setWeightSum(2f);
+        llay1.setBackgroundResource(R.drawable.border_shadow);
+        TextView text=new TextView(this);
+        text.setText("Test version");
+        llay1.addView(text);
+        root.addView(llay1);
+    }
+
+    //function for bulb1 on/off
+    public void bulb1_func(View view){
+        bulb1.setText("ON");
+       add_layout();
+    }
+
+    //function for bulb2 on/off
+    public void bulb2_func(View view){
+    bulb2.setText("ON");
+    }
+
+    public void Home_func(View view){
+        //starting another activity..
+        Intent it4 = new Intent(Setting.this, MainActivity.class);
+        startActivity(it4);
+    }
+}
